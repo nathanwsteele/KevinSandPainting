2cb1ca043d3ce62aaa9a3f6f0eb96430acd59a77
Nathan <nathan@mashupmedia.net>
2018-07-01 13:02:57 -0500

    Got bootstrap and font awesome

--- fastshell/src/js/scripts.js
+++ fastshell/src/js/scripts.js
@@ -8,3 +8,13 @@
 
 })(jQuery, window, document);
 
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    $('#access').on('touchstart click', '.skip-link', function (event) {
+      $(this).toggleClass('focus');
+      $($(this).attr('href')).toggleClass('target');
+      event.preventDefault();
+    }).find('.skip-link').append('<span>' + $('#menu .active').text() + '</span>');
+  });
+})(jQuery);
